c44af2b310d000ee6dd359df72c1c10ca92ae986
Camila Germano Rodrigues <c.germano@iesb.edu.br>
2025-03-21 10:03:03 -0300

    adicionar cards

--- doceria/App.js
+++ doceria/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, Text, Image} from 'react-native';
 import { Platform } from "react-native"; // fontes
 import { TouchableOpacity } from 'react-native'; //botão - estilizar botão
+import { StyleSheet } from "react-native";
 
 class App extends Component {
   //Estrutura do Botão
@@ -39,56 +40,75 @@ class App extends Component {
         <Text style={{fontSize: 30, color: '#c44365', textAlign:'center'}}>{this.state.nome}</Text>
         <Text style={{fontSize: 25, color: '#c44365', textAlign: 'center'}}> {nome} </Text>
 
-        <Jobs largura={200} altura={200}/>
+        <Jobs width={200} height={200}/>
+      </View>
+    );
+  }
+}
+
+// Componente do card para exibir as informações
+const Card = ({ imagem, nome, preco }) => {
+  return (
+    <View style={styles.card}>
+      <Image
+        source={{uri: imagem}} 
+        style={{width: '100%', height: 150, borderRadius: 10}} 
+      />
+      <Text style={styles.titulo}>{nome}</Text>
+      <Text style={styles.descricao}>Preço: {preco}</Text>
+    </View>
+  );
+};
+
+// Estilos
+const styles = StyleSheet.create({
+  card: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#FFEDFA',
+    elevation: 7,
+    width: 220,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
+  },
+  titulo: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#c2516f',
+    marginTop: 10,
+  },
+  descricao: {
+    fontSize: 14,
+    color: '#c2516f',
+    marginTop: 5,
+  },
+  bordacard: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'green',
+  },
+});
+
+// Componente para exibir os produtos
+class Jobs extends Component {
+  render() {
+    let imgCupcake = 'https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+    let imgDonut = 'https://images.pexels.com/photos/4686962/pexels-photo-4686962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+    let imgMacaron = 'https://img.freepik.com/fotos-gratis/tres-macarons-de-sabor-variado_198174-26.jpg?t=st=1741373747~exp=1741377347~hmac=8f4fad8e8cafbadef8bb13824da5862b91419d955a8d3881e9b7041a52888952&w=740';
+    let imgCookie = 'https://img.freepik.com/fotos-gratis/bolachas-de-chocolate_1401-441.jpg?t=st=1741377189~exp=1741380789~hmac=563079b0519473c86489f9aa23df6159579da5f7837160c66f948b2828e7b2e7&w=740';
+    let imgBolo = 'https://images.pexels.com/photos/18160775/pexels-photo-18160775/free-photo-of-cheesecake-de-morango.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+
+    return(
+      <View style={{flexDirection: 'column', flexWrap: 'wrap', alignItems: "center", justifyContent: "center"}}>
+        <Card imagem={imgCupcake} nome="Cupcake" preco="R$ 5,00" />
+        <Card imagem={imgDonut} nome="Donuts" preco="R$ 4,00" />
+        <Card imagem={imgMacaron} nome="Macarons" preco="R$ 2,50" />
+        <Card imagem={imgCookie} nome="Cookie" preco="R$ 3,00" />
+        <Card imagem={imgBolo} nome="Fatia de bolo" preco="R$ 10,00" />
       </View>
     );
   }
 }
 
 export default App;
-
-// Processo de Carregamento rápido da aplicação
-class Jobs extends Component {
-  render() {
-      let imgCupcake = 'https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-      let imgDonut = 'https://images.pexels.com/photos/4686962/pexels-photo-4686962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-      let imgMacaron = 'https://img.freepik.com/fotos-gratis/tres-macarons-de-sabor-variado_198174-26.jpg?t=st=1741373747~exp=1741377347~hmac=8f4fad8e8cafbadef8bb13824da5862b91419d955a8d3881e9b7041a52888952&w=740';
-      let imgCookie = 'https://img.freepik.com/fotos-gratis/bolachas-de-chocolate_1401-441.jpg?t=st=1741377189~exp=1741380789~hmac=563079b0519473c86489f9aa23df6159579da5f7837160c66f948b2828e7b2e7&w=740'
-      let imgBolo = 'https://images.pexels.com/photos/18160775/pexels-photo-18160775/free-photo-of-cheesecake-de-morango.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-
-      return(
-        <View style={{ alignItems: 'center' }}>
-          <Image
-          source={{uri: imgCupcake}} style={{width: this.props.largura, height: this.props.altura}}
-          />
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center', fontWeight: 'bold', textDecorationLine: 'underline' }}>Cupcake</Text>
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center', fontFamily:'Ovo'}}>Preço: R$ 5,00</Text>
-
-          <Image
-          source={{uri: imgDonut}} style={{width: this.props.largura, height: this.props.altura}}
-          />
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center', fontWeight: 'bold', textDecorationLine: 'underline'}}>Donuts</Text>
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center'}}>Preço: R$ 4,00</Text>
-
-          <Image
-          source={{uri: imgMacaron}} style={{width: this.props.largura, height: this.props.altura}}
-          />
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center', fontWeight: 'bold', textDecorationLine: 'underline'}}>Macarons</Text>
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center'}}>Preço: R$ 2,50</Text>
-
-          <Image
-          source={{uri: imgCookie}} style={{width: this.props.largura, height: this.props.altura}}
-          />
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center', fontWeight: 'bold', textDecorationLine: 'underline'}}>Cookie</Text>
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center'}}>Preço: R$ 3,00</Text>
-
-          <Image
-          source={{uri: imgBolo}} style={{width: this.props.largura, height: this.props.altura}}
-          />
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center', fontWeight: 'bold', textDecorationLine: 'underline'}}>Fatia de bolo</Text>
-          <Text style={{color: '#c2516f', fontSize: 18, margin: 10, textAlign:'center'}}>Preço: R$ 10,00</Text>
-        </View>
-        
-      );
-  }
-}
