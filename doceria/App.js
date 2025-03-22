6a1a4623b6650cf9e2971d807ebbca7ca18ea067
Camila Germano Rodrigues <c.germano@iesb.edu.br>
2025-03-22 13:36:11 -0300

    adicionar fonte dos titulos

--- doceria/App.js
+++ doceria/App.js
@@ -1,58 +1,57 @@
-import React, { Component } from "react";
-import { View, Text, Image, ScrollView, FlatList} from 'react-native';
-import { TouchableOpacity } from 'react-native'; //botão - estilizar botão
-import { StyleSheet } from "react-native";
-import { TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, ScrollView, FlatList, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Pacifico_400Regular } from '@expo-google-fonts/pacifico';
+import AppLoading from 'expo-app-loading';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: '',
-      mensagem: ''
-    };
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    PacificoRegular: Pacifico_400Regular,
+  });
+
+  const [nome, setNome] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
-  entrar = () => {
-    if (this.state.nome.trim() !== '') {
-      this.setState({ mensagem: `Seja bem-vindo(a) ao menu da doceria, ${this.state.nome}!` });
+  const entrar = () => {
+    if (nome.trim() !== '') {
+      setMensagem(`Seja bem-vindo(a) ao menu da doceria, ${nome}!`);
     } else {
-      this.setState({ mensagem: 'Por favor, insira seu nome!' });
+      setMensagem('Por favor, insira seu nome!');
     }
   };
 
-  render() {
-    return (
-      <View style={{ marginTop: 25, alignItems: 'center' }}>
-        <Text style={styles.tituloPrincipal}>Doceria</Text>
-        <Text style={styles.subTitulo}>Menu doceria</Text>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', paddingTop: 20 }}>
+      <Text style={styles.tituloPrincipal}>Confeitaria Sweet Vibes</Text>
+      <Text style={styles.subTitulo}>Menu:</Text>
 
-        {/* Campo para inserir nome */}
-        <TextInput
-          style={styles.input}
-          placeholder="Digite seu nome"
-          onChangeText={(text) => this.setState({ nome: text })}
-          value={this.state.nome}
-        />
+      {/* Campo para inserir nome */}
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome"
+        onChangeText={setNome}
+        value={nome}
+      />
 
-        {/* Botão de entrada */}
-        <TouchableOpacity style={styles.botao} onPress={this.entrar}>
-          <Text style={styles.textoBotao}>Entrar</Text>
-        </TouchableOpacity>
+      {/* Botão de entrada */}
+      <TouchableOpacity style={styles.botao} onPress={entrar}>
+        <Text style={styles.textoBotao}>Entrar</Text>
+      </TouchableOpacity>
 
-        {/* Mensagem de boas-vindas */}
-        {this.state.mensagem !== '' && (
-          <Text style={styles.mensagem}>{this.state.mensagem}</Text>
-        )}
+      {/* Mensagem de boas-vindas */}
+      {mensagem !== '' && <Text style={styles.mensagem}>{mensagem}</Text>}
 
-        <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1 }}>
-        <Jobs width={200} height={200}/>
-          <Lista />
-        </ScrollView>
-      </View>
-    );
-  }
-}
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <Jobs />
+        <Lista />
+      </ScrollView>
+    </View>
+  );
+};
 
 // Componente do card para exibir as informações
 const Card = ({ imagem, nome, preco }) => {
@@ -66,70 +65,62 @@ const Card = ({ imagem, nome, preco }) => {
 };
 
 // Componente para exibir os produtos
-class Jobs extends Component {
-  render() {
-    let produtos = [
-      { nome: "Cupcake", preco: "R$ 5,00", imagem: "https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-      { nome: "Donuts", preco: "R$ 4,00", imagem: "https://images.pexels.com/photos/4686962/pexels-photo-4686962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-      { nome: "Macarons", preco: "R$ 2,50", imagem: "https://img.freepik.com/fotos-gratis/tres-macarons-de-sabor-variado_198174-26.jpg?w=740" },
-      { nome: "Cookie", preco: "R$ 3,00", imagem: "https://img.freepik.com/fotos-gratis/bolachas-de-chocolate_1401-441.jpg?w=740" },
-      { nome: "Fatia de bolo", preco: "R$ 10,00", imagem: "https://images.pexels.com/photos/18160775/pexels-photo-18160775/free-photo-of-cheesecake-de-morango.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" }
-    ];
+const Jobs = () => {
+  const produtos = [
+    { nome: "Cupcake", preco: "R$ 5,00", imagem: "https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg" },
+    { nome: "Donuts", preco: "R$ 4,00", imagem: "https://images.pexels.com/photos/4686962/pexels-photo-4686962.jpeg" },
+    { nome: "Macarons", preco: "R$ 2,50", imagem: "https://img.freepik.com/fotos-gratis/tres-macarons-de-sabor-variado_198174-26.jpg" },
+    { nome: "Cookie", preco: "R$ 3,00", imagem: "https://img.freepik.com/fotos-gratis/bolachas-de-chocolate_1401-441.jpg" },
+    { nome: "Fatia de bolo", preco: "R$ 10,00", imagem: "https://images.pexels.com/photos/18160775/pexels-photo-18160775.jpeg" }
+  ];
 
-    return (
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        {produtos.map((item, index) => (
-          <Card key={index} imagem={item.imagem} nome={item.nome} preco={item.preco} />
-        ))}
-      </View>
-    );
-  }
-}
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      {produtos.map((item, index) => (
+        <Card key={index} imagem={item.imagem} nome={item.nome} preco={item.preco} />
+      ))}
+    </View>
+  );
+};
 
 // Componente para exibir os clientes
-class Lista extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      feed: [
-        { id: '1', nome: 'João', profissao: 'Confeiteiro' },
-        { id: '2', nome: 'Maria', profissao: 'Confeiteira' },
-        { id: '3', nome: 'José', profissao: 'Caixa' },
-        { id: '4', nome: 'Ana', profissao: 'Garçonete' }
-      ]
-    };
-  }
+const Lista = () => {
+  const feed = [
+    { id: '1', nome: 'João', profissao: 'Confeiteiro' },
+    { id: '2', nome: 'Maria', profissao: 'Confeiteira' },
+    { id: '3', nome: 'José', profissao: 'Caixa' },
+    { id: '4', nome: 'Ana', profissao: 'Garçonete' }
+  ];
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.feed}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.listaItem}>
-              <Text style={styles.nomeLista}>{item.nome} - {item.profissao}</Text>
-            </View>
-          )}
-        />
-      </View>
-    );
-  }
-}
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={feed}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.listaItem}>
+            <Text style={styles.nomeLista}>{item.nome} - {item.profissao}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
 
 // Estilos
 const styles = StyleSheet.create({
   tituloPrincipal: {
     color: '#B03052',
     fontSize: 35,
-    margin: 10,
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily: 'PacificoRegular',
   },
   subTitulo: {
     color: '#B03052',
     fontSize: 28,
     margin: 10,
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily: 'PacificoRegular',
   },
   input: {
     width: 200,
@@ -202,8 +193,7 @@ const styles = StyleSheet.create({
   nomeLista: {
     fontSize: 16,
     color: '#c2516f',
-    alignItems: "center",
-    justifyContent: "center",
+    textAlign: "center",
   }
 });
 
